2e70a8bea7e8834e0034109d6210dc9b389f8e54
Yogesh Galav <mr.yogesh.galav@gmail.com>
2026-03-13 22:03:03 +0530

    migration and seeder fix

--- database/migrations/2026_03_10_173401_add_role_id_to_users_table.php
+++ database/migrations/2026_03_10_173401_add_role_id_to_users_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->foreignId('role_id')->constrained()->onDelete('cascade');
-            $table->foreignId('company_id')->constrained()->onDelete('cascade');
+            $table->foreignId('company_id')->nullable()->constrained()->onDelete('cascade');
         });
     }
 
